0b468f6c53062afb4158b4d1ebb42809337374c4
GrokFan <developer.umarbek@gmail.com>
2025-03-11 19:01:36 +0600

    add createUser and getUser methods

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
+type User struct {
+	Name string `json:"name"`
+}
+
+var userCashe = make(map[int]User)
+
+var casheMutex sync.RWMutex
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
+	mux.HandleFunc("POST /users", createUser)
+	mux.HandleFunc("GET /users/{id}", getUser)
+	// mux.HandleFunc("De /users/{id}", getUser)
+
 
 	fmt.Println("Server listening to :8080")
 
@@ -17,5 +31,60 @@ func main() {
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if user.Name == "" {
+		http.Error(w, "name is required", http.StatusBadRequest)
+		return
+	}
+
+	casheMutex.Lock()
+
+	userCashe[len(userCashe ) + 1] = user
+
+	casheMutex.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request){
+	id, err := strconv.Atoi(r.PathValue("id")) 
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	casheMutex.RLock()
+
+	user, ok := userCashe[id]
+
+	casheMutex.RUnlock()
+
+	if !ok {
+		http.Error(w, "user not found", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader("Content-Type":"application/json")
+	j, err := json.Marshal(user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(j)
 
 }
