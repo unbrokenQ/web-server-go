c15ea0f1ae3b3a8ad6b6852c895089c2639fe50d
GrokFan <developer.umarbek@gmail.com>
2025-03-11 18:45:49 +0600

    add server

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRoot)
 
-	
+	fmt.Println("Server listening to :8080")
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
 
 }
